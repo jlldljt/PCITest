00f396e33d2c1830c2153e74ed2af3d8d8ac52bf
jlldljt <jlldljt@163.com>
2020-08-07 22:00:12 +0800

    200807

---
+++ vb com/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(67, 79)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "读1个字"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(153, 79)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "写1个字"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(42, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(188, 43)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(5, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "addr"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "value"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(7, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(94, 20)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(213, 10)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "打开com"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(107, 12)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(94, 20)
+        Me.ComboBox2.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(293, 114)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "读写保持寄存器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ vb com/Form1.vb
@@ -0,0 +1,213 @@
+﻿
+Public Class Form1
+    'Public Const GENERIC_WRITE = &H40000000
+    'Public Const GENERIC_READ = &H80000000
+    'Const FILE_ATTRIBUTE_NORMAL = &H80
+    'Const CREATE_ALWAYS = 2
+    'Const OPEN_EXISTING = 3
+    'Const OPEN_ALWAYS = 4
+    'Const INVALID_HANDLE_VALUE = -1
+    'Const FILE_NAME = "COM4"     'This can be any file that does not
+    ''currently exist.
+
+    'Structure MyType
+    '    Dim value As Integer
+    'End Structure
+
+
+    'Declare Function ReadFile Lib "kernel32" (ByVal hFile As Long, _
+    '   ByRef lpBuffer As Object, ByVal nNumberOfBytesToRead As Long, _
+    '   lpNumberOfBytesRead As Long, ByVal lpOverlapped As Long) As Long
+
+    'Private Declare Function CloseHandle Lib "kernel32" ( _
+    '  ByVal hObject As Long) As Long
+
+    'Private Declare Function WriteFile Lib "kernel32" ( _
+    '  ByVal hFile As Long, lpBuffer As Any, _
+    '  ByVal nNumberOfBytesToWrite As Long, _
+    '  lpNumberOfBytesWritten As Long, ByVal lpOverlapped As Long) As Long
+
+    'Private Declare Function CreateFile Lib "kernel32" _
+    '  Alias "CreateFileA" (ByVal lpFileName As String, _
+    '  ByVal dwDesiredAccess As Long, ByVal dwShareMode As Long, _
+    '  ByVal lpSecurityAttributes As Long, _
+    '  ByVal dwCreationDisposition As Long, _
+    '  ByVal dwFlagsAndAttributes As Long, ByVal hTemplateFile As Long) _
+    '  As Long
+
+    'Declare Function FlushFileBuffers Lib "kernel32" ( _
+    '  ByVal hFile As Long) As Long
+
+    Dim Addressreg_crc As Integer
+    Dim HiByte As Integer
+    Dim LoByte As Double
+    Dim k As Integer
+    Dim com As Long
+    Dim RcvStr(100) As Byte
+
+    Function crc16(ByRef cmdstring() As Byte, ByVal j As Integer)
+        Dim data As Integer
+        Dim i As Integer
+
+
+        Addressreg_crc = &HFFFF
+        For i = 0 To j
+            Addressreg_crc = Addressreg_crc Xor cmdstring(i)
+            For j = 0 To 7
+                data = Addressreg_crc And &H1
+                If data Then
+                    Addressreg_crc = Int(Addressreg_crc / 2)
+                    Addressreg_crc = Addressreg_crc And &H7FFF
+                    Addressreg_crc = Addressreg_crc Xor &HA001
+                Else
+                    Addressreg_crc = Addressreg_crc / 2
+                    Addressreg_crc = Addressreg_crc And &H7FFF
+                End If
+            Next j
+        Next i
+
+        If Addressreg_crc < 0 Then
+            Addressreg_crc = Addressreg_crc - &HFFFF0000
+        End If
+
+        HiByte = Addressreg_crc And &HFF
+        LoByte = (Addressreg_crc And &HFF00) / &H100
+        Return Addressreg_crc
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim SendStr(8) As Byte
+        Dim addr As UShort
+        Dim value As UShort
+        addr = Val(TextBox1.Text)
+        'Dim RcvStr() As Byte
+        Dim CmdLenth As Integer
+        SendStr(0) = 1 '从站号是1
+        SendStr(1) = &H3 '读多个字的命令代码
+        SendStr(2) = (addr - addr Mod 256) / 256 And &HFF '起始地址高字节
+        SendStr(3) = addr And &HFF '起始地址低字节
+        SendStr(4) = &H0 '数据长度高字节
+        SendStr(5) = 1 '数据长度低字节
+        Call crc16(SendStr, 5) 'CRC 计算
+        SendStr(6) = HiByte
+        SendStr(7) = LoByte
+        '读命令发送后,当接收 5 + SendStr(5) * 2 个字节时产生中断
+        CmdLenth = 5 + SendStr(5) * 2
+
+        Call send(SendStr, 8)
+
+        Threading.Thread.Sleep(1000)
+        Dim recvlen As UShort
+        recvlen = recv(RcvStr, CmdLenth)
+
+        If recvlen > 0 Then
+            value = RcvStr(3) * 256 + RcvStr(4)
+            TextBox2.Text = Str(value)
+            MessageBox.Show("成功")
+        Else
+            MessageBox.Show("失败")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim WriteStr() As Byte
+        Dim addr As UShort
+        Dim value As UShort
+        addr = Val(TextBox1.Text)
+        value = Val(TextBox2.Text)
+        k = 2 '写6 个字节
+        ReDim WriteStr(8 + k)
+        WriteStr(0) = 1 '从站号是1
+        WriteStr(1) = &H10 '写多个字的命令代码
+        WriteStr(2) = (addr - addr Mod 256) / 256 And &HFF '起始地址高字节
+        WriteStr(3) = addr And &HFF '起始地址低字节
+        WriteStr(4) = &H0 '数据长度高字节<字的个数>
+        WriteStr(5) = k / 2 '数据长度低字节<字的个数>
+        WriteStr(6) = k '数据长度<字节的个数>
+        Dim mid As UShort
+        'mid = (value - value Mod 256) / 256 '整型除出小数再四舍五入，不是我们想看到的
+        WriteStr(7) = (value - value Mod 256) / 256 And &HFF '写的第1 个字的高字节
+        WriteStr(8) = value And &HFF '写的第1 个字的低字节
+        'WriteStr(7) = &H12 '写的第1 个字的高字节
+        'WriteStr(8) = &H34 '写的第1 个字的低字节
+        Call crc16(WriteStr, 6 + k)
+        WriteStr(9) = HiByte
+        WriteStr(10) = LoByte
+
+        Call send(WriteStr, 11)
+        Threading.Thread.Sleep(1000)
+        Dim recvlen As UShort
+        recvlen = recv(RcvStr, 8)
+
+        If recvlen > 0 Then
+            MessageBox.Show("成功")
+        Else
+            MessageBox.Show("失败")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'com = CreateFile(FILE_NAME, GENERIC_WRITE Or GENERIC_READ, _
+        '                     0, 0, OPEN_EXISTING, 0, 0)
+
+        GetSerialPortNames()
+        GetSerialBaudRate()
+    End Sub
+
+    Function recv(buf() As Byte, ByVal num As Integer)
+        Dim recvlen As UShort
+        recvlen = SerialPort1.Read(buf, 0, num)
+        Return recvlen
+
+        'For i As Int16 = 0 To bytesRead - 1
+        '    indata = indata & DecToHex(ch(i))
+        'Next
+    End Function
+
+    Sub send(buf() As Byte, ByVal num As Integer)
+        'serialPort1.WriteLine(str);
+        SerialPort1.DiscardOutBuffer()
+        SerialPort1.DiscardInBuffer()
+        SerialPort1.Write(buf, 0, num)
+        'MessageBox()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Sub GetSerialPortNames()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+        If ComboBox1.Items.Count > 0 Then
+            ComboBox1.SelectedIndex = 0
+        End If
+    End Sub
+
+    Sub GetSerialBaudRate()
+
+        ComboBox2.Items.Add(9600)
+        ComboBox2.Items.Add(19200)
+        ComboBox2.Items.Add(115200)
+
+        If ComboBox2.Items.Count > 0 Then
+            ComboBox2.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+        End If
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = Val(ComboBox2.Text)
+        SerialPort1.Parity = IO.Ports.Parity.None '奇偶校验
+        SerialPort1.DataBits = 8 '数据位
+        SerialPort1.StopBits = IO.Ports.StopBits.One '停止位
+        SerialPort1.Open()
+        MessageBox.Show("打开" + ComboBox1.Text)
+    End Sub
+End Class
